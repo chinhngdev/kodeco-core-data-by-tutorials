64c5438472efaaf7bb726a311661f56083108200
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-08-10 16:06:03 +0700

    Initialize HitList Core Data iOS project

---
+++ 01-your-first-core-data-app/projects/starter/HitList/HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Chinh on 8/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
