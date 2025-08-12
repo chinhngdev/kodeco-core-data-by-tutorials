7c72623527d98165ffc83787d18816de64e462f2
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-08-12 16:59:26 +0700

    Add BowTie NSManagedObject subclass and properties
    - Introduces BowTie+CoreDataClass.swift and BowTie+CoreDataProperties.swift for Core Data entity management.
    - Updates AppDelegate to test saving and fetching BowTie objects, and modifies the data model to remove explicit code generation type.

---
+++ 02-nsmanagedobject-subclasses/projects/starter/BowTies/BowTies/CoreDataEntities/BowTie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BowTie+CoreDataClass.swift
+//  BowTies
+//
+//  Created by Chinh on 8/12/25.
+//  Copyright © 2025 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BowTie)
+public class BowTie: NSManagedObject {
+
+}
---
+++ 02-nsmanagedobject-subclasses/projects/starter/BowTies/BowTies/CoreDataEntities/BowTie+CoreDataProperties.swift
@@ -0,0 +1,36 @@
+//
+//  BowTie+CoreDataProperties.swift
+//  BowTies
+//
+//  Created by Chinh on 8/12/25.
+//  Copyright © 2025 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+
+extension BowTie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<BowTie> {
+        return NSFetchRequest<BowTie>(entityName: "BowTie")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var lastWorn: Date?
+    @NSManaged public var name: String?
+    @NSManaged public var photoData: Data?
+    @NSManaged public var rating: Double
+    @NSManaged public var searchKey: String?
+    @NSManaged public var timesWorn: Int32
+    @NSManaged public var tintColor: UIColor?
+    @NSManaged public var url: URL?
+
+}
+
+extension BowTie : Identifiable {
+
+}
