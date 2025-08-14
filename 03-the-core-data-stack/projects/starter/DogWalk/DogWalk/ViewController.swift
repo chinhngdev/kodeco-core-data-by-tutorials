7365baed9f91769513d600774c9e645a90fd229c
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2025-08-14 15:01:50 +0700

    Refactor to use Core Data for walks management
    - Replaces the walks array with Core Data entities, associating walks with a Dog object.
    - Implements fetching, creating, and deleting walks using Core Data, and updates table view data source methods accordingly.

--- 03-the-core-data-stack/projects/starter/DogWalk/DogWalk/ViewController.swift
+++ 03-the-core-data-stack/projects/starter/DogWalk/DogWalk/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
   
   lazy var coreDataStack: CoreDataStack = CoreDataStack(modelName: "DogWalk")
 
-  var walks: [Date] = []
+  var currentDog: Dog?
 
   // MARK: - IBOutlets
   @IBOutlet var tableView: UITableView!
@@ -55,35 +55,105 @@ class ViewController: UIViewController {
     super.viewDidLoad()
 
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    
+    let dogName = "Fido"
+    let dogFetch: NSFetchRequest<Dog> = Dog.fetchRequest()
+    dogFetch.predicate = NSPredicate(format: "%K == %@",
+                                    #keyPath(Dog.name), dogName)
+
+    do {
+        let results = try coreDataStack.managedContext.fetch(dogFetch)
+        if results.isEmpty {
+            // Fido not found, create Fido
+            currentDog = Dog(context: coreDataStack.managedContext)
+            currentDog?.name = dogName
+            coreDataStack.saveContext()
+        } else {
+            // Fido found, use Fido
+            currentDog = results.first
+        }
+    } catch let error as NSError {
+        print("Fetch error: \(error) description: \(error.userInfo)")
+    }
   }
 }
 
 // MARK: - IBActions
 extension ViewController {
   @IBAction func add(_ sender: UIBarButtonItem) {
-    walks.append(Date())
+    // Insert a new Walk entity into Core Data
+    let walk = Walk(context: coreDataStack.managedContext)
+    walk.date = Date()
+    
+    // Insert the new Walk into the Dog's walks set
+//    if let dog = currentDog,
+//       let walks = dog.walks?.mutableCopy()
+//        as? NSMutableOrderedSet {
+//      walks.add(walk)
+//      dog.walks = walks
+//    }
+    currentDog?.addToWalks(walk)
+    // Save the managed object context
+    coreDataStack.saveContext()
+    
+    // Reload table view
     tableView.reloadData()
   }
 }
 
 // MARK: UITableViewDataSource
 extension ViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    walks.count
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int {
+    return currentDog?.walks?.count ?? 0
   }
-
+  
   func tableView(
     _ tableView: UITableView,
     cellForRowAt indexPath: IndexPath
   ) -> UITableViewCell {
-    let date = walks[indexPath.row]
     let cell = tableView.dequeueReusableCell(
       withIdentifier: "Cell", for: indexPath)
-    cell.textLabel?.text = dateFormatter.string(from: date)
+    
+    guard let walk = currentDog?.walks?[indexPath.row] as? Walk,
+          let walkDate = walk.date as Date? else {
+      return cell
+    }
+    
+    cell.textLabel?.text = dateFormatter.string(from: walkDate)
     return cell
   }
-
+  
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     "List of Walks"
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    canEditRowAt indexPath: IndexPath
+  ) -> Bool {
+    true
+  }
+  
+  func tableView(
+    _ tableView: UITableView,
+    commit editingStyle: UITableViewCell.EditingStyle,
+    forRowAt indexPath: IndexPath
+  ) {
+    //1
+    guard let walkToRemove =
+            currentDog?.walks?[indexPath.row] as? Walk,
+          editingStyle == .delete else {
+      return
+    }
+    
+    //2
+    coreDataStack.managedContext.delete(walkToRemove)
+    
+    //3
+    coreDataStack.saveContext()
+    
+    //4
+    tableView.deleteRows(at: [indexPath], with: .automatic)
   }
 }
